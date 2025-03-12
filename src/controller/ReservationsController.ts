dfc90e785b0de3a906c6cd9570f086701a6afd84
willy031110 <411630063@o365.tku.edu.tw>
2025-03-12 08:18:01 +0800

    0312

--- src/controller/ReservationsController.ts
+++ src/controller/ReservationsController.ts
@@ -13,10 +13,27 @@ export class ReservationsController extends Contorller {
         super();
         this.service = new PageService();
     }
-
     public async test(Request: Request, Response: Response) {
         await DB.connection?.query("USE lab_b310;");
-        const resp = await DB.connection?.query("SELECT * FROM Reservations;");
+        const sql = `
+        SELECT 
+            r.reservation_id, 
+            s.student_id, 
+            s.student_name, 
+            se.row_label, 
+            se.seat_number, 
+            t.start_time, 
+            t.end_time, 
+            r.create_time
+        FROM Reservations r
+        LEFT JOIN Students s ON r.student_id = s.student_id
+        LEFT JOIN Seats se ON r.seat_id = se.seat_id
+        LEFT JOIN Timeslots t ON r.timeslot_id = t.timeslot_id
+        ORDER BY r.create_time;
+    `;
+
+        const resp = await DB.connection?.query(sql);
         Response.send(resp)
     }
-}
+    
+    }
